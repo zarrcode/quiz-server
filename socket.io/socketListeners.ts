950f99d4d5e2083a4fb640f79d96b7ea5b5abc55
Zou Minowa <zouminowa@gmail.com>
2022-02-02 20:24:54 +0000

    refactor(socket.io): move game_join logic to separate file

--- socket.io/socketListeners.ts
+++ socket.io/socketListeners.ts
@@ -1,46 +1,14 @@
 import { Server } from 'socket.io';
 import { type UserSocket } from './interfaces';
-import { destroySession, addGameIDToSession } from './TEMP/sessionStoreTEMP';
-import { gameExists } from './TEMP/stubsTEMP';
-import { getGameRoomByID, getUsersInRoom } from './helper';
 import gameCreateHandler from './gameHandlers/gameCreateHandler';
+import gameJoinHandler from './gameHandlers/gameJoinHandler';
 
 export function addSocketListeners(io: Server, socket: UserSocket) {
   // for developement purposes
   socket.onAny((event, ...args) => console.log(event, ...args));
 
   socket.on('game_create', (options) => gameCreateHandler(io, socket, options));
-
-  socket.on('game_join', (gameID: string) => {
-    try {
-      if (!gameExists(gameID)) {
-        destroySession(socket.sessionID!); // TODO: replace with call to Angus function
-        // emit custom 'error' to trigger disconnection on client
-        const reason = 'game does not exist';
-        socket.emit('disconnect_custom', reason);
-      }
-
-      // join user to game
-      addGameIDToSession(socket.sessionID!, gameID); // TODO: replace with call to Angus function
-      socket.join(gameID);
-
-      // send all users in room
-      const room = getGameRoomByID(io, gameID);
-      const users = getUsersInRoom(io, room!);
-      socket.emit('users', users);
-
-      // alert other users
-      const user = { username: socket.username };
-      socket.to(gameID).emit('users_join', user);
-    } catch (err) {
-      console.error(err);
-
-      destroySession(socket.sessionID!); // TODO: replace with call to Angus function
-      // emit custom 'error' to trigger disconnection on client
-      const reason = 'failed to join game';
-      socket.emit('disconnect_custom', reason);
-    }
-  });
+  socket.on('game_join', (gameID) => gameJoinHandler(io, socket, gameID));
 
   socket.on('game_start', () => {
     // TODO: get question from model
