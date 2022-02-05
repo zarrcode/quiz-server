e2a52e62d975bc4aa28a612bb9968b29a98e02b1
Zou Minowa <zouminowa@gmail.com>
2022-02-05 16:22:35 +0000

    feat(socket.io): do stuff

--- socket.io/events/usersLeaveEvent.ts
+++ socket.io/events/usersLeaveEvent.ts
@@ -1,6 +1,10 @@
-import { UserSocket } from '../interfaces';
+import { type UserSocket } from '../interfaces';
+// import { getGameRoomByID, getSocketsInRoom } from '../helperFunctions';
 
 export default function usersLeaveEvent(socket: UserSocket) {
   const { sessionID } = socket;
-  socket.broadcast.emit('users_leave', sessionID); // TODO: just braodcase to room
+  // const { gameID } = socket;
+  // const room = getGameRoomByID(gameID);
+  // const sockets = getSocketsInRoom()
+  socket.broadcast.emit('users_leave', sessionID); // TODO: just braodcast to room
 }
