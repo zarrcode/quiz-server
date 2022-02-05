a6bac0f8cc860ea73c655be25aa8889a14a3f144
Ross <rossllewellyn95@hotmail.co.uk>
2022-02-05 16:54:39 +0000

    finalCorrectAnswers and updateCorrectAnswers Handlers

---
+++ socket.io/handlers/updateCorrectAnswersHandler.ts
@@ -0,0 +1,20 @@
+import { type Server } from 'socket.io';
+import { type UserSocket } from '../interfaces';
+import { getGameRoomByID, getSocketsInRoom } from '../helperFunctions';
+
+export default function finalCorrectAnswersHandler(io: Server, socket: UserSocket) {
+  socket.on('correct_answers', async (gameID, correctAnswers) => {
+    try {
+      const room = getGameRoomByID(io, gameID);
+      if (room) {
+        const sockets = getSocketsInRoom(io, room);
+        sockets.forEach((socket) => socket.emit('corrected_answers', correctAnswers));
+      }
+    } catch (err) {
+      console.error(err);
+      // emit custom 'error'
+      const reason = 'failed to submit correct answer';
+      socket.emit('answer_error', reason);
+    }
+  });
+}
