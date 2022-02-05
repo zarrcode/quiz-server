f940a97479944700576ee8dfb8c9f34cb9acb1e3
Angus Edwards <angus.s.edwards@gmail.com>
2022-02-05 15:04:59 +0000

    updating replace html texted answers

--- models/answers.ts
+++ models/answers.ts
@@ -39,15 +39,18 @@ export const evaluateAnswer = async (
   answer: string,
 ) => {
   try {
+    await client.hSet(gameID, 'Gamestate', 'answers');
     const quiz = await client.hGetAll(gameID);
     const currentQuestionNumber = quiz.Current_Question;
-    const correctAnswer = quiz[`Question${currentQuestionNumber}[answer]`].replace(/&#039;/g, "'").replace(/&quot;/g, '"').replace(/&shy;/g, '-');
+    const correctAnswer = quiz[`Question${currentQuestionNumber}[answer]`].replace(/&#039;/g, "'").replace(/&quot;/g, '"').replace(/&shy;/g, '-').replace(/&[rl]dquo;/g, '"')
+      .replace(/&rsquo;/g, "'")
+      .replace(/&amp;/g, '&');
     // eslint-disable-next-line max-len
     const similarity = stringSimilarity.compareTwoStrings(correctAnswer.toLowerCase(), answer.toLowerCase());
     // if (similarity > 0.656) updateScoreboard(gameID, username);
     addToAnswerList(gameID, username, answer, correctAnswer, similarity);
     const current = await client.hIncrBy(gameID, 'Submitted_Answers', 1);
-    if (current >= Number(quiz.Active_Players)) return true;
+    if (current >= Number(quiz.Active_Players)) { return true; }
     return false;
   } catch (err) {
     return err;
