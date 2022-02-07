a021c2746ce2faf9d7d988e806f38ec6bd38d582
Zou Minowa <zouminowa@gmail.com>
2022-02-07 19:26:13 +0000

    feat(socket.io): fix stuff

--- socket.io/events/usersLeaveEvent.ts
+++ socket.io/events/usersLeaveEvent.ts
@@ -1,15 +1,19 @@
 import { type Server } from 'socket.io';
 import { type UserSocket } from '../interfaces';
 import { getGameRoomByID, getSocketsInRoom } from '../helperFunctions';
-// import { getGameRoomByID, getSocketsInRoom } from '../helperFunctions';
 
 export default function usersLeaveEvent(io: Server, socket: UserSocket) {
   const { sessionID } = socket;
   const { gameID } = socket;
-  const room = <Set<string>>getGameRoomByID(io, gameID!);
-  const sockets = getSocketsInRoom(io, room);
+  if (gameID) {
+    const room = <Set<string>>getGameRoomByID(io, gameID!);
 
-  sockets.forEach((socket) => {
-    socket.emit('users_leave', sessionID);
-  });
+    if (room) {
+      const sockets = getSocketsInRoom(io, room);
+
+      sockets.forEach((socket) => {
+        socket.emit('users_leave', sessionID);
+      });
+    }
+  }
 }
--- socket.io/handlers/gameEndHandler.ts
+++ socket.io/handlers/gameEndHandler.ts
@@ -6,9 +6,7 @@ import { disconnectSocketsInGame } from '../helperFunctions';
 export default function gameEndHandler(io: Server, socket: UserSocket) {
   socket.on('game_end', async () => {
     const { gameID } = socket;
-    console.log(gameID);
     if (gameID) {
-      console.log('about to destory game');
       await destroyGame(gameID);
       disconnectSocketsInGame(io, gameID);
     }
