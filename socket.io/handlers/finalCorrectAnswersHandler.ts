859cd183b5d0b76724cd8f22f6613a212efe3151
Ross <rossllewellyn95@hotmail.co.uk>
2022-02-07 16:09:39 +0000

    updateScoreState

--- socket.io/handlers/finalCorrectAnswersHandler.ts
+++ socket.io/handlers/finalCorrectAnswersHandler.ts
@@ -1,12 +1,13 @@
 import { type Server } from 'socket.io';
 import { type UserSocket } from '../interfaces';
-import { renderScoreboard, updateScoreboard } from '../../models/scoreboard';
+import { renderScoreboard, updateScoreboard, updateScoreState } from '../../models/scoreboard';
 import { getGameRoomByID, getSocketsInRoom } from '../helperFunctions';
 
 export default function finalCorrectAnswersHandler(io: Server, socket: UserSocket) {
   socket.on('final_correct_answers', async (gameID, correctAnswers) => {
     try {
       await updateScoreboard(gameID, correctAnswers);
+      await updateScoreState(gameID);
       const scoreboard = await renderScoreboard(gameID);
       const room = getGameRoomByID(io, gameID);
       if (room) {
