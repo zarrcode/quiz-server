dec81391084d3ca1eb14e1722f907d6be3a5e9b4
Angus Edwards <angus.s.edwards@gmail.com>
2022-02-07 09:25:18 +0000

    updating decoder

--- models/answers.ts
+++ models/answers.ts
@@ -1,5 +1,6 @@
 import client from '../db';
 import { updateScoreboard } from './scoreboard';
+import { decode } from 'html-entities';
 
 const stringSimilarity = require('string-similarity');
 
@@ -42,9 +43,7 @@ export const evaluateAnswer = async (
     await client.hSet(gameID, 'Gamestate', 'answers');
     const quiz = await client.hGetAll(gameID);
     const currentQuestionNumber = quiz.Current_Question;
-    const correctAnswer = quiz[`Question${currentQuestionNumber}[answer]`].replace(/&#039;/g, "'").replace(/&quot;/g, '"').replace(/&shy;/g, '-').replace(/&[rl]dquo;/g, '"')
-      .replace(/&rsquo;/g, "'")
-      .replace(/&amp;/g, '&');
+    const correctAnswer = decode(quiz[`Question${currentQuestionNumber}[answer]`]);
     // eslint-disable-next-line max-len
     const similarity = stringSimilarity.compareTwoStrings(correctAnswer.toLowerCase(), answer.toLowerCase());
     // if (similarity > 0.656) updateScoreboard(gameID, username);
