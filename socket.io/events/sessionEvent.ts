fd137167fd492f9d8ac4157583001ccc1435c0e3
Zou Minowa <zouminowa@gmail.com>
2022-02-07 13:28:52 +0000

    fix(socket.io): fix joining errors

--- socket.io/events/sessionEvent.ts
+++ socket.io/events/sessionEvent.ts
@@ -1,10 +1,5 @@
 import { type UserSocket } from '../interfaces';
 
 export default function sessionEvent(socket: UserSocket) {
-  const session = {
-    sessionID: socket.sessionID,
-    username: socket.username,
-    gameID: socket.gameID,
-  };
-  socket.emit('session', session);
+  socket.emit('session', socket.sessionID);
 }
