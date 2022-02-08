0bdc181c1462b777a12a738cf5779e651ffa0713
Ross <rossllewellyn95@hotmail.co.uk>
2022-02-08 19:15:07 +0000

    do stuff

--- db.ts
+++ db.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-  url: 'redis://:p2049463d4cc86eb13dab1abd7eda7d91f9991bd2dc1d572853ad4e7861718519@ec2-34-251-79-166.eu-west-1.compute.amazonaws.com:30780',
+  url: process.env.REDIS_URL,
 });
 
 client.connect();
