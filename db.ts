5c05583199e5bd78e9b36797ccd12582d46b80ac
Ross <rossllewellyn95@hotmail.co.uk>
2022-02-08 18:30:32 +0000

    do stuff

--- db.ts
+++ db.ts
@@ -4,9 +4,9 @@ const client = createClient({
   url: 'redis://:p2049463d4cc86eb13dab1abd7eda7d91f9991bd2dc1d572853ad4e7861718519@ec2-34-251-79-166.eu-west-1.compute.amazonaws.com:30780',
 });
 
+client.connect();
+
 client.on('error', (err) => console.log('Redis Connection Error', err));
 console.log('connection to client');
-
-client.connect();
 
 export default client;
